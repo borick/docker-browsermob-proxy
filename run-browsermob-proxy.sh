bc48f1d9d06e9879a96f0f9c434e05261e708278
Boris Sitsker <bsitsker@eyereturn.com>
2016-02-25 10:10:18 -0500

    remove ttl

--- run-browsermob-proxy.sh
+++ run-browsermob-proxy.sh
@@ -2,4 +2,4 @@
 
 JAVA_OPTS=${JAVA_OPTS:="-Xmx1024m"} \
 /browsermob-proxy-2.1.0-beta-4/bin/browsermob-proxy \
-${BMP_OPTS:="-port 9090 --use-littleproxy true -proxyPortRange 9091-9121 -ttl 60"}
+${BMP_OPTS:="-port 9091 --use-littleproxy true -proxyPortRange 9092-9092"}
